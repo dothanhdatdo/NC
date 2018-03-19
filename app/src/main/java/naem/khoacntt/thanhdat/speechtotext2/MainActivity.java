abb71630a9f10959640fc6113a382a2fb76c8719
kietdv <dinhvankiet124@gmail.com>
2018-03-19 23:42:28 +0700

    Realtime

--- app/src/main/java/naem/khoacntt/thanhdat/speechtotext2/MainActivity.java
+++ app/src/main/java/naem/khoacntt/thanhdat/speechtotext2/MainActivity.java
@@ -3,8 +3,11 @@ package naem.khoacntt.thanhdat.speechtotext2;
 import android.os.Bundle;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -21,6 +24,10 @@ public class MainActivity extends AppCompatActivity {
     private final int REQ_CODE_SPEECH_INPUT = 100;
     private EditText edtOut;
 
+    private static final String TAG = "MyActivity";
+
+    private SpeechRecognizer recognizer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +35,10 @@ public class MainActivity extends AppCompatActivity {
 
         voiceInput = (TextView) findViewById(R.id.voiceInput);
         speakButton = (TextView) findViewById(R.id.btnSpeak);
+        edtOut = (EditText) findViewById(R.id.edtTextOut);
+
+        recognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        recognizer.setRecognitionListener(new Listener());
 
         speakButton.setOnClickListener(new View.OnClickListener() {
 
@@ -36,7 +47,61 @@ public class MainActivity extends AppCompatActivity {
                 askSpeechInput();
             }
         });
+    }
 
+    class Listener implements RecognitionListener {
+        public void onReadyForSpeech(Bundle params) {
+            Log.d(TAG, "onReadyForSpeech");
+        }
+
+        public void onBeginningOfSpeech() {
+            Log.d(TAG, "onBeginningOfSpeech");
+        }
+
+        public void onRmsChanged(float rmsdB) {
+            Log.d(TAG, "onRmsChanged");
+        }
+
+        public void onBufferReceived(byte[] buffer) {
+            Log.d(TAG, "onBufferReceived");
+        }
+
+        public void onEndOfSpeech() {
+            Log.d(TAG, "onEndofSpeech");
+        }
+
+        public void onError(int error) {
+            String message = "";
+
+            if (error == SpeechRecognizer.ERROR_AUDIO) message = "audio";
+            else if (error == SpeechRecognizer.ERROR_CLIENT) message = "client";
+            else if (error == SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS)
+                message = "insufficient permissions";
+            else if (error == SpeechRecognizer.ERROR_NETWORK) message = "network";
+            else if (error == SpeechRecognizer.ERROR_NETWORK_TIMEOUT) message = "network timeout";
+            else if (error == SpeechRecognizer.ERROR_NO_MATCH) message = "no match found";
+            else if (error == SpeechRecognizer.ERROR_RECOGNIZER_BUSY) message = "recognizer busy";
+            else if (error == SpeechRecognizer.ERROR_SERVER) message = "server";
+            else if (error == SpeechRecognizer.ERROR_SPEECH_TIMEOUT) message = "speech timeout";
+
+            Log.d(TAG, "error " + message);
+        }
+
+        public void onResults(Bundle results) {
+            Log.d(TAG, "onResults");
+        }
+
+        public void onPartialResults(Bundle partialResults) {
+            ArrayList<String> result = partialResults.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+            if (result != null) {
+                String word = (String) result.get(result.size() - 1);
+                edtOut.setText(word);
+            }
+        }
+
+        public void onEvent(int eventType, Bundle params) {
+            Log.d(TAG, "onEvent " + eventType);
+        }
     }
 
     // Showing google speech input dialog
@@ -46,35 +111,11 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
                 RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, "voice.recognition.test");
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
                 "Hi speak something");
-        try {
-            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
-        } catch (ActivityNotFoundException a) {
+        intent.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, true);
 
-        }
-    }
-
-    // Receiving speech input
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        edtOut = (EditText) findViewById(R.id.edtTextOut);
-
-        switch (requestCode) {
-            case REQ_CODE_SPEECH_INPUT: {
-                if (resultCode == RESULT_OK && null != data) {
-
-                    ArrayList<String> result = data
-                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    //voiceInput.setText(result.get(0));
-                    edtOut.setText(result.get(0));
-                    
-                }
-                break;
-            }
-
-        }
+        recognizer.startListening(intent);
     }
 }
